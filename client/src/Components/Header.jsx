eb028e99c2327fd50a7f0a2bac1b2e161253d57c
CoderEvoke <CodersEvoke72@gmail.com>
2024-02-01 16:08:09 +0500

    nodemon done

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
                     <FaMoon/>
                 </Button>
                 <Link to='Signin'>
-                    <Button gradientDuoTone='purpleToBlue' >
+                    <Button gradientDuoTone='purpleToBlue' outline >
                     Sign In
                     </Button>
                 </Link>
